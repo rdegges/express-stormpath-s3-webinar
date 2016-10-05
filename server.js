74272fc11b3260aad2cf831e91405ea5a76d1191
Randall Degges <r@rdegges.com>
2016-10-05 13:41:51 -0700

    Adding a server.js example.

---
+++ server.js
@@ -0,0 +1,78 @@
+"use strict";
+
+const express = require("express");
+const multer = require("multer");
+const stormpath = require("express-stormpath");
+const stormpathS3 = require("express-stormpath-s3");
+
+let app = express();
+
+let storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "/tmp/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  }
+});
+let upload = multer({ storage: storage });
+
+app.set("view engine", "pug");
+
+app.use(stormpath.init(app, {
+  client: {
+    apiKey: {
+      id: process.env.STORMPATH_CLIENT_APIKEY_ID,
+      secret: process.env.STORMPATH_CLIENT_APIKEY_SECRET
+    }
+  },
+  application: {
+    href: process.env.STORMPATH_APPLICATION_HREF
+  },
+  expand: {
+    customData: true
+  }
+}));
+app.use(stormpath.getUser);
+app.use(stormpathS3({
+  awsAccessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  awsBucket: "express-stormpath-s3"
+}));
+
+app.get("/", stormpath.loginRequired, (req, res) => {
+  res.render("upload");
+});
+
+app.post("/", stormpath.loginRequired, upload.single("file"), (req, res, next) => {
+  let fileName = req.file.originalname;
+  let path = req.file.path;
+
+  req.user.uploadFile(path, "public-read", (err) => {
+    if (err) {
+      return next(err);
+    }
+
+    res.send("File available <a href=\"" + req.user.customData.s3[fileName].href + "\">here</a>.");
+  });
+});
+
+app.get("/files", stormpath.loginRequired, (req, res, next) => {
+  res.render("files", { files: req.user.customData.s3 });
+});
+
+app.get("/delete", stormpath.loginRequired, (req, res, next) => {
+  if (!req.query.filename) {
+    return res.redirect("/files");
+  }
+
+  req.user.deleteFile(req.query.filename, (err) => {
+    if (err) {
+      return next(err);
+    }
+
+    res.redirect("/files");
+  });
+});
+
+app.listen(3000);
